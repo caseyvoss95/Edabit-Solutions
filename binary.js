e15f1038ae159d3455970351c663631ed8ed42a9
caseyvoss95 <casey.vos@bu.edu>
2022-11-22 23:15:21 -0500

    added additional binary tests

--- binary.js
+++ binary.js
@@ -1,25 +1,36 @@
-//Challenge Name: Convert a Number to Base-2
+//Challenge: Convert a Number to Base-2
 //https://edabit.com/challenge/3kcrnpHk7krNZdnKK
 //Purpose: convert base-10 number to binary
 //Input: Non-Negative Integer (type: number)
 //Output: A string composed of 1's and 0's that represents a binary number
 //        Returns false if input is anything other than a non-negative number
+
 function binary(decimal) {
+    //check for invalid input
     if (decimal < 0 || typeof decimal != "number") {
         return false;
     }
+    
+    //check for special cases
     if (decimal < 2){
           return decimal.toString();
       }
-    let result = [];
-      let place = 2
-      while (decimal >= place) {
+    
+    //build binary string
+    let result = []; 
+    let place = 2 //base 2 establishsed
+
+    //find highest digit
+    while (decimal >= place) {
           place = place * 2;
       }
+    
+    //push first digit
     place = place / 2;
     let sum = place
     result.push('1');
   
+    //push remaining digits
     while (place != 1){
       place = place / 2;
       if (sum + place <= decimal){
--- test/binary_test.js
+++ test/binary_test.js
@@ -1,9 +1,13 @@
+//chai dependencies
 const chai = require('chai');
-const binary = require('../script')
 const assert = chai.assert;
 
+//functions for testing
+const binary = require('../binary')
+
+//binary(decimbal) tests
 describe('binary function', ()=> {
-    it('works for edge-cases', (done) => {
+    it('returns expected value for edge-cases', (done) => {
         assert.equal(binary(0), '0');
         assert.equal(binary(1), '1');
         assert.equal(binary(2), '10');
@@ -16,8 +20,24 @@ describe('binary function', ()=> {
         assert.equal(binary(new Date()), false);
         done();
     });
-    it('works for large numbers', (done) => {
-        assert.equal(binary(1024), '10000000000');
+    it('returns expected value for a sequence of ascending integers', (done) => {
+        assert.equal(binary(57), '111001');
+        assert.equal(binary(58), '111010');
+        assert.equal(binary(59), '111011');
+        assert.equal(binary(60), '111100');
+        assert.equal(binary(61), '111101');
+        assert.equal(binary(62), '111110');
+        assert.equal(binary(63), '111111');
+        assert.equal(binary(64), '1000000');
+        assert.equal(binary(65), '1000001');
         done();
-    })
+    });
+    it('returns expected value for larger numbers', (done) => {
+        assert.equal(binary(512), '1000000000');
+        assert.equal(binary(1024), '10000000000');
+        assert.equal(binary(1025), '10000000001');
+        assert.equal(binary(3010), '101111000010');
+        assert.equal(binary(337796), '1010010011110000100');
+        done();
+    });
 })
