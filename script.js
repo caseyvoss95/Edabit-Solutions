963a02efc527ff67e3e7004a0813178dc9f5d5a2
caseyvoss95 <casey.vos@bu.edu>
2022-11-22 22:58:44 -0500

    binary edge-tests added

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+//Challenge Name: Convert a Number to Base-2
+//https://edabit.com/challenge/3kcrnpHk7krNZdnKK
 //Purpose: convert base-10 number to binary
 //Input: Non-Negative Integer (type: number)
 //Output: A string composed of 1's and 0's that represents a binary number
+//        Returns false if input is anything other than a non-negative number
 function binary(decimal) {
+    if (decimal < 0 || typeof decimal != "number") {
+        return false;
+    }
     if (decimal < 2){
           return decimal.toString();
       }
--- test/binary_test.js
+++ test/binary_test.js
@@ -7,7 +7,17 @@ describe('binary function', ()=> {
         assert.equal(binary(0), '0');
         assert.equal(binary(1), '1');
         assert.equal(binary(2), '10');
-        assert.equal(binary(3), '11');
+        assert.equal(binary(-1), false);
+        assert.equal(binary('foo'), false);
+        assert.equal(binary({}), false);
+        assert.equal(binary({'foo': 'bar'}), false);
+        assert.equal(binary([]), false);
+        assert.equal(binary([17, 48]), false);
+        assert.equal(binary(new Date()), false);
+        done();
+    });
+    it('works for large numbers', (done) => {
+        assert.equal(binary(1024), '10000000000');
         done();
     })
 })
